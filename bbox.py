2db09cf7c7f540888bb988c5914411cc0e3b01e9
RyanX <looe_xiao@hotmail.com>
2024-06-12 17:31:07 +0800

    Refine IoU calculation

--- bbox.py
+++ bbox.py
@@ -48,33 +48,44 @@ def sanity_fix(box):
         
     return box
 
+
+def unify_boxcoords(box):
+    # Adjust box coord to upper-left point + bottom-right point.
+    x1, y1, x2, y2 = box[:, 0], box[:, 1], box[:, 2], box[:, 3]
+    box[:, 0] = torch.min(x1, x2)
+    box[:, 1] = torch.min(y1, y2)
+    box[:, 2] = torch.max(x1, x2)
+    box[:, 3] = torch.max(y1, y2)
+    return box
+
+
 def bbox_iou(box1, box2):
     """
     Returns the IoU of two bounding boxes 
-    
-    
     """
-    #Get the coordinates of bounding boxes
-    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
-    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    # Adjust box coord to upper-left point + bottom-right point
+    unify_boxcoords(box1)
+    unify_boxcoords(box2)
+
+    # Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+    # Get the coordinates of the intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
     
-    #get the corrdinates of the intersection rectangle
-    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
-    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
-    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
-    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
-    
-    #Intersection area
-    if torch.cuda.is_available():
-            inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape).cuda())*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape).cuda())
-    else:
-            inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape))*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape))
-    
-    #Union Area
+    # Intersection area
+    inter_area = (inter_rect_x2 - inter_rect_x1 + 1) * (inter_rect_y2 - inter_rect_y1 + 1)
+
+    # Union Area
     b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
     b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
-    
-    iou = inter_area / (b1_area + b2_area - inter_area)
+    union_area = b1_area + b2_area - inter_area
+
+    iou = inter_area / union_area
     
     return iou
 
